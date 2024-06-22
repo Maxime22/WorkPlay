f0746f6da2323127fb76a3598b9ee7519a268c45
Alexandre <alexandre@Alexandres-MacBook-Pro.local>
2024-06-22 18:28:02 +0200

    Ajouter Tailwind

--- App.tsx
+++ App.tsx
@@ -3,13 +3,8 @@ import {Text, View} from 'react-native';
 
 const WorkPlayApp = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, Lulu!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text>Hello, Maxou!</Text>
     </View>
   );
 };
