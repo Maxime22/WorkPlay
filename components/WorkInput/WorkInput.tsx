b6de26a3b83c070eb5e030836c8ea84ec73c3a3a
Maxime22 <maxime.sabouret@evaneos.com>
2024-07-10 07:44:05 +0200

    feat: add handleRatio in WorkInput

--- components/WorkInput/WorkInput.tsx
+++ components/WorkInput/WorkInput.tsx
@@ -1,12 +1,15 @@
-import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {styles} from './WorkInput.style.ts';
+import {TimeDifficultyRatio} from '../TimeDifficultyRatio/TimeDifficultyRatio.tsx';
 
 type WorkInputProps = {
   inputTitle: string;
   id: string;
   deleteInput: () => void;
   timeActivity: string;
+  ratio: string;
+    handleRatioChange: (id: string, ratio: string) => void;
   onTimeActivityChange: (id: string, value: string) => void;
   isDisabled: boolean;
 };
@@ -15,9 +18,12 @@ export const WorkInput = ({
   id,
   deleteInput,
   timeActivity,
+  ratio,
+                              handleRatioChange,
   onTimeActivityChange,
   isDisabled,
 }: WorkInputProps) => {
+
   return (
     <View key={id} style={styles.inputContainer}>
       <Text style={styles.inputTitle}>{inputTitle}</Text>
@@ -29,6 +35,10 @@ export const WorkInput = ({
         value={timeActivity}
         onChangeText={text => onTimeActivityChange(id, text)}
         editable={!isDisabled}
+      />
+      <TimeDifficultyRatio
+        selectedRatio={ratio}
+        onRatioChange={value=> handleRatioChange(id, value)}
       />
       <TouchableOpacity onPress={deleteInput} style={styles.deleteButton}>
         <Text style={styles.deleteButtonText}>Delete</Text>
